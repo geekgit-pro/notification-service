ca8fe3208508b91c8ef118791ea6fa998cc77db9
Aijaz <geeksen7@gmail.com>
2024-10-14 20:07:49 +0530

    Implemented sub model for notification-service & completed pub-sub with sendEmail

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const { PORT } = require('./config');
-const { ServerConfig, Logger } = require('./config')
+const { ServerConfig, Logger } = require('./config');
+const { EmailService } = require('./services');
+const amqplib = require('amqplib');
 const app = express();
 const apiRoutes = require('./routes');
 const mailSender = require('./config/email-config');
+const { data } = require('./utils/common/error-response');
+
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+        await channel.assertQueue('noti-queue');
+        channel.consume('noti-queue', async (data)=> {
+            //console.log(`${Buffer.from(data.content)}`);
+            console.log(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            // const object = JSON.parse(data);
+            await EmailService.sendEmail('notify.air.service@gmail.com', object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -13,7 +34,7 @@ const router = express.Router();
 app.use('/api', apiRoutes);
 
 
-app.listen(ServerConfig.PORT, ()=> {
+app.listen(ServerConfig.PORT, async ()=> {
     console.log(`Successfully started the server at ${ServerConfig.PORT}`);
     Logger.info('Server is live', {});
     // try {
@@ -27,5 +48,7 @@ app.listen(ServerConfig.PORT, ()=> {
     // } catch (error) {
     //     console.log(error);
     // }
+    await connectQueue();
+    console.log('Queue is up');
 
 });
